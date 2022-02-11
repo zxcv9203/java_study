17cf1ccaaf0565d828a9bb3f62f1be03d848cf15
zxcv9203 <zxcv9203@naver.com>
2022-02-12 02:05:42 +0900

    algorithm

---
+++ src/algorithm/programmers/level2/MenuRenewal.java
@@ -0,0 +1,14 @@
+package algorithm.programmers.level2;
+
+import java.util.HashMap;
+
+public class MenuRenewal {
+    static String[] solution(String[] orders, int[] course) {
+        HashMap<String, Integer> setMenu = new HashMap<>();
+
+        return answer;
+    }
+    public static void main(String[] args) {
+
+    }
+}
