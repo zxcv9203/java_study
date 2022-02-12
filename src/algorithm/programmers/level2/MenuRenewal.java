3d0538a86846b6f0d2ac00bbf820231fb5da35ce
zxcv9203 <zxcv9203@naver.com>
2022-02-12 11:56:36 +0900

    algorithm

--- src/algorithm/programmers/level2/MenuRenewal.java
+++ src/algorithm/programmers/level2/MenuRenewal.java
@@ -1,14 +1,65 @@
 package algorithm.programmers.level2;
 
-import java.util.HashMap;
+import java.util.*;
 
 public class MenuRenewal {
+    static boolean[] used;
+    static void newMenu(HashMap<String, Integer> menuList, int course, String menu, String sett, int idx) {
+        if (sett.length() > course) {
+            return ;
+        }
+        if (sett.length() == course) {
+            int cnt = 1;
+            char[] settToChar = sett.toCharArray();
+            Arrays.sort(settToChar);
+            sett = new String(settToChar);
+            if (menuList.get(sett) != null) {
+                cnt += menuList.get(sett);
+            }
+            menuList.put(sett, cnt);
+            return ;
+        }
+        for (int i = idx; i < menu.length(); i++) {
+            int using = menu.charAt(i) - 'A';
+            String tmp = String.valueOf(menu.charAt(i));
+            if (used[using])
+                continue;
+            used[using] = true;
+            newMenu(menuList, course, menu, sett + tmp, i);
+            used[using] = false;
+        }
+    }
     static String[] solution(String[] orders, int[] course) {
-        HashMap<String, Integer> setMenu = new HashMap<>();
+        LinkedList<String> answer = new LinkedList<>();
+        for (int c : course) {
+            HashMap<String, Integer> menuList = new HashMap<>();
+            int big = 0;
+            for (String order : orders) {
+                used = new boolean[26];
+                newMenu(menuList, c, order, "", 0);
+            }
+            for (Integer cnt : menuList.values()) {
+                big = Math.max(cnt, big);
+            }
+            if (big <= 1)
+                continue;
+            for (Map.Entry<String, Integer> entry : menuList.entrySet()) {
 
-        return answer;
+                if (big == entry.getValue()) {
+                    answer.add(entry.getKey());
+                }
+            }
+        }
+        Collections.sort(answer);
+        return answer.toArray(new String[0]);
     }
     public static void main(String[] args) {
-
+        String[] orders = {
+                "ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"
+        };
+        int[] course = {
+                2,3,4
+        };
+        System.out.println(Arrays.toString(solution(orders, course)));
     }
 }
---
+++ src/java_jungsuk/chap06/DefaultParameterExam.java
@@ -0,0 +1,18 @@
+package java_jungsuk.chap06;
+
+public class DefaultParameterExam {
+    public static void main(String[] args) {
+        Data d = new Data();
+        d.x = 10;
+        System.out.println("main() : x = " + d.x);
+        change(d.x);
+        System.out.println("After change(d.x)");
+        System.out.println("main() : x = " + d.x);
+    }
+    static void change(int x) {
+        x = 1000;
+        System.out.println("change() : x = " + x);
+    }
+}
+
+class Data { int x; }
