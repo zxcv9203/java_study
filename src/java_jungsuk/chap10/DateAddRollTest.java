52d29438378214b87c2a4e63b30221cd941e77f9
zxcv9203 <zxcv9203@naver.com>
2022-03-05 22:48:28 +0900

    calendar test

---
+++ src/java_jungsuk/chap10/DateAddRollTest.java
@@ -0,0 +1,33 @@
+package java_jungsuk.chap10;
+
+import java.util.Calendar;
+
+public class DateAddRollTest {
+    public static String toString(Calendar date) {
+        return date.get(Calendar.YEAR) + "년 " + (date.get(Calendar.MONTH) + 1) + "월" +
+                date.get(Calendar.DATE) + "일";
+    }
+
+    public static void main(String[] args) {
+        Calendar date = Calendar.getInstance();
+        date.set(2019, Calendar.AUGUST, 31);
+        System.out.println(toString(date));
+
+        System.out.println("1일 후");
+        date.add(Calendar.DATE, 1);
+        System.out.println(toString(date));
+
+        System.out.println("6달 전");
+        date.add(Calendar.MONTH, -6);
+        System.out.println(toString(date));
+
+        System.out.println("31일 후 (roll)");
+        date.roll(Calendar.DATE, 31);
+        System.out.println(toString(date));
+
+        System.out.println("31일 후 (add)");
+        date.add(Calendar.DATE, 31);
+        System.out.println(toString(date));
+
+    }
+}
