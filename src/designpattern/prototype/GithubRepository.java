0c7e6044a68a7db4059eddabbb5725a8e1519115
yongckim <zxcv9203@naver.com>
2022-04-24 03:55:14 +0900

    designpattern : prototype

---
+++ src/designpattern/prototype/GithubRepository.java
@@ -0,0 +1,22 @@
+package designpattern.prototype;
+
+public class GithubRepository {
+    private String user;
+    private String name;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
